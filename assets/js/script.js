33a4f39e65f8dd6c8b6ac4958bca7e53aa54a887
Keith <keithvip@gmail.com>
2022-03-23 13:57:43 -0500

    fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,8 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-primary").click(function(event) {
+    event.preventDefault();
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
